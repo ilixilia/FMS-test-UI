f61bf67b437adf5a04d61f6ab3e521e803afd55d
Elena Gorshkova <44033158+ilixilia@users.noreply.github.com>
2019-04-24 17:14:52 +0300

    pages 1, 2 and 5
    Pages 1, 2 and 5

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+	
+	// to go to another scene by its name
+	public void ChangeToScene (string goToScene) {
+        Application.LoadLevel(goToScene);
+	}
+}
