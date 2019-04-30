619d7b1d914706bc3d54ce75007cb0815e74a53a
Elena Gorshkova <44033158+ilixilia@users.noreply.github.com>
2019-05-01 00:35:16 +0300

    Added pages 3, 4 and 6

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+    public GameObject playWindow;
+
+    // to go to another scene by its name
+    public void ChangeToScene (string goToScene) {
+        Application.LoadLevel(goToScene);
+	}
+
+    public void playVideo()
+    {
+
+        playWindow.SetActive(true);
+
+    }
+}
