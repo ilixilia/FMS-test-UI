1fd5d87a05944db55c9df9be15abe67943549b77
Elena Gorshkova <ilixilia@gmail.com>
2019-05-06 09:42:57 +0300

    vslkdjl

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
-    public string goToScene;
-
-     // to go to another scene by its name
-    public void ChangeToScene (string goToScene) {
-
+    // to go to another scene by its name
+    public void ChangeToScene(string goToScene)
+    {
         SceneManager.LoadScene(goToScene);
         //Application.LoadLevel(goToScene);
-	}
-
+    }
 }
